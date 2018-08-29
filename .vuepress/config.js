8f27b0955d5c414b8df680af4bcbd02349288f0b
pranksteess <xhxpecer@gmail.com>
2018-08-29 19:13:28 +0800

    [新增]-系统合约接口说明

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -34,6 +34,7 @@ function genSidebarConfig() {
             children: [
                 '',
                 'ram1',
+				'system_contract_API_introduction_v1.2.1.md'
             ]
         }
     ]
