67bcee2fae42d57b170281e7b298635ad0c8c4a7
longhuicode <longhuicode@foxmail.com>
2018-08-29 12:56:14 +0800

    [新增]新增ga

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,6 +5,8 @@ module.exports = {
     dest: "./dist",
     head: [
         ['link', {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css'}],
+        ['script', {type: 'text/javascript', async: 'async', src: 'https://www.googletagmanager.com/gtag/js?id=UA-120212654-2'}],
+        ['script', {src: './ga.js'}]
     ],
     themeConfig: {
         nav: [
