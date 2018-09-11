9c8e0c66c800924893a6989b584ce0ec99e6c7d2
pranksteess <xhxpecer@gmail.com>
2018-09-12 02:12:28 +0800

    [修复]-配置文件语法错误

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -36,7 +36,7 @@ function genSidebarConfig() {
                 'ram1',
                 'ram2',
                 'bid',
-				'system_contract_API_introduction_v1.2.1'
+				'system_contract_API_introduction_v1.2.1',
 				'vote'
             ]
         }
